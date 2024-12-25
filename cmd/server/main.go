6ee9e0b6ba244613d48148ff1cd89db6f7fa89ff
Andrew Garetov <andrew1gar@yandex.ru>
2024-12-25 14:41:23 +0300

    #1 Вывод информации о текущей версии

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/AndrXxX/goph-keeper/pkg/buildformatter"
+	"github.com/AndrXxX/goph-keeper/pkg/logger"
+)
+
+var buildVersion string
+var buildDate string
+var buildCommit string
+
+func main() {
+	if iErr := logger.Initialize("Info"); iErr != nil {
+		log.Fatal(iErr)
+	}
+	buildFormatter := buildformatter.BuildFormatter{
+		Labels: []string{"Build version", "Build date", "Build commit"},
+		Values: []string{buildVersion, buildDate, buildCommit},
+	}
+	for _, bInfo := range buildFormatter.Format() {
+		logger.Log.Info(bInfo)
+	}
 }
