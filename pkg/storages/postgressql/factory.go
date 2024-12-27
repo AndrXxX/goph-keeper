329c0d450f33284a87bd59ff3fe28e0c24b189f4
Andrew Garetov <andrew1gar@yandex.ru>
2024-12-27 19:16:11 +0300

    #1 Добавлена документация

--- pkg/storages/postgressql/factory.go
+++ pkg/storages/postgressql/factory.go
@@ -6,10 +6,12 @@ import (
 	"github.com/AndrXxX/goph-keeper/pkg/storages/postgressql/models"
 )
 
+// Factory фабрика для хранилищ моделей
 type Factory struct {
 	DB *igor.Database
 }
 
+// UsersStorage хранилище для модели User
 func (f *Factory) UsersStorage() *Storage[models.User] {
 	return &Storage[models.User]{f.DB}
 }
