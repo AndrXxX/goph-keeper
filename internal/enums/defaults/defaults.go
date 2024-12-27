d9b94036099e6ed2ec72ac35b0f0c5d633e1b684
Andrew Garetov <andrew1gar@yandex.ru>
2024-12-27 19:37:54 +0300

    #1 Добавлен пакет defaults

---
+++ internal/enums/defaults/defaults.go
@@ -0,0 +1,9 @@
+package defaults
+
+const RunAddress = "localhost:8080"
+const LogLevel = "info"
+
+// AuthKeyExpired - время в секундах (по умолчанию 1 час)
+const AuthKeyExpired = 1 * 60 * 60
+const AuthKey = "auth-secret-key"
+const PasswordKey = "password-secret-key"
