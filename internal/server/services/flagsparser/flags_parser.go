bf50c3d361f66bbcc39bd1a0d436ad81128b0883
Andrew Garetov <andrew1gar@yandex.ru>
2024-12-29 15:27:28 +0300

    #1 Добавлен FlagsParser

---
+++ internal/server/services/flagsparser/flags_parser.go
@@ -0,0 +1,22 @@
+package flagsparser
+
+import (
+	fl "flag"
+
+	"github.com/AndrXxX/goph-keeper/internal/server/config"
+)
+
+// FlagsParser сервис для парсинга аргументов командной строки
+type FlagsParser struct {
+}
+
+// Parse парсит аргументы командной строки и наполняет конфигурацию
+func (p FlagsParser) Parse(c *config.Config) error {
+	fl.StringVar(&c.Host, "a", c.Host, "Net address host:port")
+	fl.StringVar(&c.DatabaseURI, "d", c.DatabaseURI, "Database URI")
+	fl.StringVar(&c.AuthKey, "ak", c.AuthKey, "Auth Key")
+	fl.IntVar(&c.AuthKeyExpired, "ake", c.AuthKeyExpired, "Auth Key expired")
+	fl.StringVar(&c.PasswordKey, "pk", c.PasswordKey, "Password Key")
+	fl.Parse()
+	return nil
+}
