9023a3e5f8a6aa1c2980a96e44b5d72343310512
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-02 23:36:48 +0300

    #1 Доработка Storage

--- pkg/storages/postgressql/storage.go
+++ pkg/storages/postgressql/storage.go
@@ -13,44 +13,43 @@ type Storage[T igor.DBModel] struct {
 	db *igor.Database
 }
 
-func (s *Storage[T]) Find(m T) (T, error) {
-	var resModel T
-	err := s.db.Model(m).Where(m).Scan(resModel)
-	if err != nil {
-		logger.Log.Info("find model", zap.Error(err), zap.Any("model", m))
-		return resModel, err
+func (s *Storage[T]) Find(m *T) (*T, error) {
+	var res []T
+	_ = s.db.Model(*new(T)).Where(*m).Scan(&res)
+	if len(res) == 0 {
+		return nil, nil
 	}
-	return resModel, nil
+	return &res[0], nil
 }
 
-func (s *Storage[T]) Create(m T) (T, error) {
-	err := s.db.Model(m).Create(m)
+func (s *Storage[T]) Create(m *T) (*T, error) {
+	err := s.db.Create(*m)
 	if err != nil {
-		logger.Log.Info("create model", zap.Error(err), zap.Any("model", m))
+		logger.Log.Info("create model", zap.Error(err), zap.Any("model", *m))
 		return m, err
 	}
 	return m, nil
 }
 
-func (s *Storage[T]) Update(m T) error {
-	err := s.db.Model(m).Updates(m)
-	if err.Error != nil {
-		logger.Log.Info("update model", zap.Error(err), zap.Any("model", m))
+func (s *Storage[T]) Update(m *T) error {
+	err := s.db.Updates(*m)
+	if err != nil {
+		logger.Log.Info("update model", zap.Error(err), zap.Any("model", *m))
 		return err
 	}
 	return nil
 }
 
-func (s *Storage[T]) List(m T, updatedFrom *time.Time) ([]T, error) {
+func (s *Storage[T]) List(m *T, updatedFrom *time.Time) ([]T, error) {
 	var l []T
-	q := s.db.Model(m)
+	q := s.db.Model(*m)
 	if updatedFrom != nil {
 		q = q.Where("updated_at > ?", updatedFrom)
 	}
 	err := q.Scan(&l)
-	if err.Error != nil {
+	if err != nil {
 		logger.Log.Info("fetch list models", zap.Error(err), zap.Any("model", m))
-		return l, err
+		return nil, err
 	}
 	return l, nil
 }
