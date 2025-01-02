898e80a65026e0ab1db94fb757ea11eb4177d268
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-03 00:29:40 +0300

    #1 Получение токена из заголовков

--- internal/server/api/middlewares/is_authorized_middleware.go
+++ internal/server/api/middlewares/is_authorized_middleware.go
@@ -2,7 +2,9 @@ package middlewares
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"go.uber.org/zap"
 
@@ -16,15 +18,20 @@ type isAuthorized struct {
 
 func (m *isAuthorized) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token, err := r.Cookie(enums.AuthToken)
-		if err != nil {
-			logger.Log.Info("failed to get auth token cookie", zap.Error(err))
-			w.WriteHeader(http.StatusUnauthorized)
-			return
+		token := m.getTokenFromHeaders(r)
+		if token == "" {
+			t, err := m.getTokenFromCookie(r)
+			if err != nil {
+				logger.Log.Info("get auth token cookie", zap.Error(err))
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			token = t
 		}
-		userID, err := m.ts.Decrypt(token.Value)
+
+		userID, err := m.ts.Decrypt(token)
 		if err != nil {
-			logger.Log.Info("failed to decrypt token from cookie", zap.Error(err))
+			logger.Log.Info("decrypt token", zap.Error(err))
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
@@ -32,6 +39,24 @@ func (m *isAuthorized) Handler(next http.Handler) http.Handler {
 		ctx = context.WithValue(ctx, enums.UserID, userID)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func (m *isAuthorized) getTokenFromCookie(r *http.Request) (string, error) {
+	t, err := r.Cookie(enums.AuthToken)
+	if err != nil {
+		return "", fmt.Errorf("getTokenFromCookie %w", err)
+	}
+	return t.Value, nil
+}
+
+func (m *isAuthorized) getTokenFromHeaders(r *http.Request) string {
+	if raw := r.Header.Get("Authorization"); raw != "" {
+		vals := strings.Split(raw, " ")
+		if len(vals) == 2 {
+			return vals[1]
+		}
+	}
+	return ""
 }
 
 func IsAuthorized(ts tokenService) *isAuthorized {
