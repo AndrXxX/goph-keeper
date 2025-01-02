cd26c427086037daea34cc834d1e108464b81403
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-02 23:18:27 +0300

    #1 Добавлен метод FetchSlice

--- pkg/requestjsonentity/request_json_entity_fetcher.go
+++ pkg/requestjsonentity/request_json_entity_fetcher.go
@@ -23,3 +23,18 @@ func (c *Fetcher[T]) Fetch(r io.Reader) (*T, error) {
 	}
 	return entity, nil
 }
+
+func (c *Fetcher[T]) FetchSlice(r io.Reader) ([]*T, error) {
+	var list []*T
+	dec := json.NewDecoder(r)
+	err := dec.Decode(&list)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode request: %w", err)
+	}
+	for _, item := range list {
+		if _, err := govalidator.ValidateStruct(item); err != nil {
+			return nil, fmt.Errorf("failed to validate request: %w", err)
+		}
+	}
+	return list, nil
+}
