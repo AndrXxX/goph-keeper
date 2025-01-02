0cf1f338d69cb2c341faeaefc17dd3e692ce4c27
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-02 11:29:58 +0300

    #1 Добавлена модель StoredItem

---
+++ pkg/storages/postgressql/models/stored_item.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+)
+
+type StoredItem struct {
+	ID          uint      `igor:"primary_key"`
+	CreatedAt   time.Time `sql:"default:(now() at time zone 'utc')"`
+	UpdatedAt   time.Time `sql:"default:(now() at time zone 'utc')"`
+	Type        string
+	Description string
+	Value       string
+	UserId      uint
+}
+
+func (u StoredItem) TableName() string {
+	return "stored_items"
+}
