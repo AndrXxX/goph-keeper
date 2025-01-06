1dd035dd64c738f9c1ff66b8c38971afeb43ec68
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-06 18:31:40 +0300

    #1 Добавлен Container

---
+++ internal/client/views/container.go
@@ -0,0 +1,94 @@
+package views
+
+import (
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
+	kb "github.com/AndrXxX/goph-keeper/internal/client/keyboard"
+	"github.com/AndrXxX/goph-keeper/internal/client/messages"
+	"github.com/AndrXxX/goph-keeper/internal/client/views/names"
+)
+
+var margin = 5
+var containerKeys = kb.KeyMap{
+	Short: []key.Binding{kb.Quit},
+	Full:  [][]key.Binding{{kb.Quit}},
+}
+
+type Container struct {
+	help     help.Model
+	loaded   bool
+	current  names.ViewName
+	views    Map
+	quitting bool
+}
+
+func NewContainer(cols Map) *Container {
+	return &Container{help: help.New(), views: cols}
+}
+
+func (m *Container) Init() tea.Cmd {
+	m.current = names.PasswordList //TODO
+	return nil
+}
+
+func (m *Container) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		var cmd tea.Cmd
+		var cmds []tea.Cmd
+		m.help.Width = msg.Width - margin
+		for i := range m.views {
+			_, cmd = m.views[i].Update(msg)
+			cmds = append(cmds, cmd)
+		}
+		m.loaded = true
+		return m, tea.Batch(cmds...)
+
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, kb.Keys.Quit):
+			m.quitting = true
+			return m, tea.Quit
+		}
+	case messages.ChangeView:
+		m.current = msg.Name
+		if msg.View != nil {
+			m.views[m.current] = msg.View
+		}
+		if msg.Msg != nil {
+			_, cmd = m.views[m.current].Update(msg.Msg)
+		}
+	}
+	_, cmd = m.views[m.current].Update(msg)
+	return m, cmd
+}
+
+func (m *Container) View() string {
+	if m.quitting {
+		return m.getStyle().Render("")
+	}
+	if !m.loaded {
+		return m.getStyle().Render("loading...")
+	}
+	board := lipgloss.JoinHorizontal(
+		lipgloss.Left,
+		m.views[m.current].View(),
+	)
+	var km = containerKeys
+	if kk, ok := m.views[m.current].(keyboardKeys); ok {
+		km = kk.kbKeys()
+	}
+
+	return lipgloss.JoinVertical(lipgloss.Left, m.getStyle().Render(board), m.help.View(km))
+}
+
+func (m *Container) getStyle() lipgloss.Style {
+	return lipgloss.NewStyle().
+		Padding(1, 2).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62"))
+}
