cf34e8ba780d8c3289ea8be39a5e5f6bd7ef9000
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-08 00:19:30 +0300

    #1 Упрощение кода в passwordList

--- internal/client/views/lists/password_list.go
+++ internal/client/views/lists/password_list.go
@@ -10,6 +10,7 @@ import (
 	kb "github.com/AndrXxX/goph-keeper/internal/client/keyboard"
 	"github.com/AndrXxX/goph-keeper/internal/client/messages"
 	"github.com/AndrXxX/goph-keeper/internal/client/views/forms"
+	"github.com/AndrXxX/goph-keeper/internal/client/views/helpers"
 	"github.com/AndrXxX/goph-keeper/internal/client/views/names"
 	"github.com/AndrXxX/goph-keeper/internal/client/views/styles"
 	"github.com/AndrXxX/goph-keeper/pkg/entities"
@@ -46,6 +47,7 @@ func (pl *passwordList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		pl.list.SetSize(msg.Width/styles.InnerMargin, msg.Height/2)
 	case messages.AddPassword:
+		// TODO: action
 		pl.list.InsertItem(-1, msg.Item)
 		pl.View()
 		return pl, nil
@@ -55,28 +57,15 @@ func (pl *passwordList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if len(pl.list.VisibleItems()) != 0 {
 				e := pl.list.SelectedItem().(*entities.PasswordItem)
 				f := forms.NewPasswordForm(e)
-				return f, func() tea.Msg {
-					return messages.ChangeView{
-						Name: names.PasswordForm,
-						View: f,
-					}
-				}
+				return f, helpers.GenCmd(messages.ChangeView{Name: names.PasswordForm, View: f})
 			}
 		case key.Matches(msg, kb.Keys.New):
 			f := forms.NewPasswordForm(nil)
-			return f, func() tea.Msg {
-				return messages.ChangeView{
-					Name: names.PasswordForm,
-					View: f,
-				}
-			}
+			return f, helpers.GenCmd(messages.ChangeView{Name: names.PasswordForm, View: f})
 		case key.Matches(msg, kb.Keys.Back):
-			return pl, func() tea.Msg {
-				return messages.ChangeView{
-					Name: names.MainMenu,
-				}
-			}
+			return pl, helpers.GenCmd(messages.ChangeView{Name: names.MainMenu})
 		case key.Matches(msg, kb.Keys.Delete):
+			// TODO: approve + action
 			return pl, pl.DeleteCurrent()
 		}
 	}
