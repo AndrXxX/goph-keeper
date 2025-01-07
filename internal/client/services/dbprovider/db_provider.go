9281102d06136760bd3160c2aecd2ce9349fb5a1
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-08 00:24:51 +0300

    #1 Добавлен метод IsDBExist

--- internal/client/services/dbprovider/db_provider.go
+++ internal/client/services/dbprovider/db_provider.go
@@ -1,15 +1,28 @@
 package dbprovider
 
 import (
+	"os"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-const path = "./app.db?_auth&_auth_user=admin&_auth_pass="
+const (
+	path   = "./app.db"
+	params = "?_auth&_auth_user=admin&_auth_pass="
+)
 
 type DBProvider struct {
 }
 
+func (p *DBProvider) IsDBExist() bool {
+	_, e := os.Stat(path)
+	if e != nil && os.IsNotExist(e) {
+		return false
+	}
+	return true
+}
+
 func (p *DBProvider) DB(masterPass string) (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open(path+masterPass), &gorm.Config{})
+	return gorm.Open(sqlite.Open(path+params+masterPass), &gorm.Config{})
 }
