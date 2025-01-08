d7b62075dbaacce2d44d1521ee3ead6638f2bf5c
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-08 13:24:01 +0300

    #1 Проброс хранилищ в адаптеры

--- internal/client/services/storageadapters/factory.go
+++ internal/client/services/storageadapters/factory.go
@@ -1,31 +1,35 @@
 package storageadapters
 
 import (
-	"github.com/AndrXxX/goph-keeper/internal/client/entities"
-	"github.com/AndrXxX/goph-keeper/internal/client/ormmodels"
+	e "github.com/AndrXxX/goph-keeper/internal/client/entities"
+	"github.com/AndrXxX/goph-keeper/internal/client/interfaces"
+	orm "github.com/AndrXxX/goph-keeper/internal/client/ormmodels"
 	"github.com/AndrXxX/goph-keeper/internal/client/services/convertors"
 )
 
 type Factory struct {
 }
 
-func (f Factory) ORMPasswordsAdapter() ORMAdapter[entities.PasswordItem, ormmodels.PasswordItem] {
-	return ORMAdapter[entities.PasswordItem, ormmodels.PasswordItem]{
+func (f Factory) ORMPasswordsAdapter(s interfaces.Storage[orm.PasswordItem]) ORMAdapter[e.PasswordItem, orm.PasswordItem] {
+	return ORMAdapter[e.PasswordItem, orm.PasswordItem]{
 		ORMConvertor:    convertors.PasswordOrmEntityConvertor{},
 		EntityConvertor: convertors.PasswordEntityOrmConvertor{},
+		Storage:         s,
 	}
 }
 
-func (f Factory) ORMNotesAdapter() ORMAdapter[entities.NoteItem, ormmodels.NoteItem] {
-	return ORMAdapter[entities.NoteItem, ormmodels.NoteItem]{
+func (f Factory) ORMNotesAdapter(s interfaces.Storage[orm.NoteItem]) ORMAdapter[e.NoteItem, orm.NoteItem] {
+	return ORMAdapter[e.NoteItem, orm.NoteItem]{
 		ORMConvertor:    convertors.NoteOrmEntityConvertor{},
 		EntityConvertor: convertors.NoteEntityOrmConvertor{},
+		Storage:         s,
 	}
 }
 
-func (f Factory) ORMBankCardAdapter() ORMAdapter[entities.BankCardItem, ormmodels.BankCardItem] {
-	return ORMAdapter[entities.BankCardItem, ormmodels.BankCardItem]{
+func (f Factory) ORMBankCardAdapter(s interfaces.Storage[orm.BankCardItem]) ORMAdapter[e.BankCardItem, orm.BankCardItem] {
+	return ORMAdapter[e.BankCardItem, orm.BankCardItem]{
 		ORMConvertor:    convertors.BankCardOrmEntityConvertor{},
 		EntityConvertor: convertors.BankCardEntityOrmConvertor{},
+		Storage:         s,
 	}
 }
