f58517975c2f67e7d6ce5cf67b15cb1c17466e3b
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-08 13:39:20 +0300

    #1 Добавлен ID в user

--- internal/client/entities/user.go
+++ internal/client/entities/user.go
@@ -1,6 +1,7 @@
 package entities
 
 type User struct {
+	ID             uint   `json:"id"`
 	Login          string `json:"login" valid:"minstringlength(3),required"`
 	Password       string `json:"password" valid:"minstringlength(5),required"`
 	Token          string
