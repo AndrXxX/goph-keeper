0b9137e2b17f9eaa0e4edd51bf9feaf2fc85779b
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-08 12:10:56 +0300

    #1 Правка dbprovider

--- internal/client/services/dbprovider/db_provider.go
+++ internal/client/services/dbprovider/db_provider.go
@@ -1,6 +1,7 @@
 package dbprovider
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/sqlite"
@@ -8,8 +9,9 @@ import (
 )
 
 const (
-	path   = "./app.db"
-	params = "?_auth&_auth_user=admin&_auth_pass="
+	path           = "./app.db"
+	authParams     = "?_auth&_auth_user=admin&_auth_pass=%s"
+	createDBParams = "?_auth&_auth_user=admin&_auth_pass=%s&_auth_crypt=sha256"
 )
 
 type DBProvider struct {
@@ -20,8 +22,13 @@ func (p *DBProvider) IsDBExist() bool {
 	return e == nil || !os.IsNotExist(e)
 }
 
+// TODO: The SQLITE_USER_AUTHENTICATION extension is deprecated
+
 func (p *DBProvider) DB(masterPass string) (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open(path+params+masterPass), &gorm.Config{})
+	if p.IsDBExist() {
+		return gorm.Open(sqlite.Open(path+fmt.Sprintf(authParams, masterPass)), &gorm.Config{})
+	}
+	return gorm.Open(sqlite.Open(path+fmt.Sprintf(createDBParams, masterPass)), &gorm.Config{})
 }
 
 func (p *DBProvider) RemoveDB() error {
