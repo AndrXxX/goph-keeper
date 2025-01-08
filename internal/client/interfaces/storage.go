a95807e2896cf00f8c4dc27b33e28b2ae17d2124
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-08 13:41:02 +0300

    #1 Доработка storageadapters Factory

--- internal/client/interfaces/storage.go
+++ internal/client/interfaces/storage.go
@@ -1,8 +1,8 @@
 package interfaces
 
 type Storage[T any] interface {
-	Find(m *T) *T
-	Create(m *T) (*T, error)
-	Update(m *T) error
-	FindAll(m *T) []T
+	Find(*T) *T
+	Create(*T) (*T, error)
+	Update(*T) error
+	FindAll(*T) []T
 }
