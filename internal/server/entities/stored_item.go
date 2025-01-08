ef58e30c8b7267fbb28e49b34f3527f4aa012e8a
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-08 22:27:44 +0300

    #1 Корректная работа с датой на сервере

--- internal/server/entities/stored_item.go
+++ internal/server/entities/stored_item.go
@@ -3,9 +3,9 @@ package entities
 import "time"
 
 type StoredItem struct {
-	ID        uint       `json:"id"`
-	Desc      string     `json:"description"`
-	UpdatedAt *time.Time `json:"updated_at"`
+	ID        uint      `json:"id"`
+	Desc      string    `json:"description"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (si StoredItem) GetID() uint {
--- internal/server/services/entityconvertors/stored_item_convertor.go
+++ internal/server/services/entityconvertors/stored_item_convertor.go
@@ -13,7 +13,7 @@ func (c storedItemConvertor) ToModel(e *entities.StoredItem, userID uint, v stri
 	return &models.StoredItem{
 		ID:          e.ID,
 		Type:        c.Type,
-		UpdatedAt:   e.UpdatedAt,
+		UpdatedAt:   &e.UpdatedAt,
 		Description: e.Desc,
 		Value:       v,
 		UserID:      userID,
@@ -24,6 +24,6 @@ func (c storedItemConvertor) ToEntity(e *models.StoredItem) *entities.StoredItem
 	return &entities.StoredItem{
 		ID:        e.ID,
 		Desc:      e.Description,
-		UpdatedAt: e.UpdatedAt,
+		UpdatedAt: *e.UpdatedAt,
 	}
 }
