e80d164f552349d60b5ec601f0f396505278519f
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-11 23:21:16 +0300

    #2 Добавлен with_token_test

---
+++ pkg/requestsender/with_token_test.go
@@ -0,0 +1,42 @@
+package requestsender
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/AndrXxX/goph-keeper/pkg/requestsender/dto"
+)
+
+func TestWithToken(t *testing.T) {
+	tests := []struct {
+		name    string
+		token   string
+		params  dto.ParamsDto
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name:    "Test with empty token",
+			token:   "",
+			params:  dto.ParamsDto{Headers: make(map[string]string)},
+			want:    map[string]string{"Authorization": "Bearer "},
+			wantErr: false,
+		},
+		{
+			name:    "Test with token test",
+			token:   "test",
+			params:  dto.ParamsDto{Headers: make(map[string]string)},
+			want:    map[string]string{"Authorization": "Bearer test"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := WithToken(tt.token)
+			require.Equal(t, tt.wantErr, f(&tt.params) != nil)
+			assert.Equal(t, tt.want, tt.params.Headers)
+		})
+	}
+}
