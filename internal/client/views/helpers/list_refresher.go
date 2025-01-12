4800563bee2d388b4c2f8f5aff6522794de3180a
Andrew Garetov <andrew1gar@yandex.ru>
2025-01-12 19:23:50 +0300

    #2 Корректное обновление списка

--- internal/client/views/helpers/list_refresher.go
+++ internal/client/views/helpers/list_refresher.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"sync/atomic"
 	"time"
 
 	"github.com/charmbracelet/bubbles/list"
@@ -10,20 +11,20 @@ import (
 type ListRefresher[T list.Item] struct {
 	S          Storage[T]
 	List       *list.Model
-	refreshing bool
+	refreshing atomic.Bool
 }
 
 func (l *ListRefresher[T]) Refresh() {
-	if l.refreshing || l.S == nil {
+	if l.refreshing.Load() || l.S == nil {
 		return
 	}
-	l.refreshing = true
+	l.refreshing.Store(true)
 	items := l.S.FindAll(nil)
 	l.List.SetItems([]list.Item{})
 	for i := range items {
 		l.List.InsertItem(intsets.MaxInt, items[i])
 	}
-	l.refreshing = false
+	l.refreshing.Store(false)
 }
 
 func (l *ListRefresher[T]) RefreshIn(t time.Duration) {
